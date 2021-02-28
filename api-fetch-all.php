34c473fca167f3dd4bd0b31d0b32808f435f779d
atul90163 <79519862+atul90163@users.noreply.github.com>
2021-02-28 18:23:44 +0530

    Add files via upload

---
+++ api-fetch-all.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type:application/json');
+header('Acess-Control-Allow-Origin: *');
+//copy("api-fetch-all.php","api-fetch-single.php");
+
+$con=mysqli_connect("localhost","root","","ajax_db") or die("unable to connect".mysqli_connect_error());
+$query=mysqli_query($con,"select * from ajax_tbl") or die("somthing_error".mysqli_error($con));
+if(mysqli_num_rows($query)> 0){
+	
+	
+$data=mysqli_fetch_all($query,MYSQLI_ASSOC);
+
+
+$json_data=json_encode($data,JSON_PRETTY_PRINT);
+echo $json_data;
+
+
+}else{
+	
+	
+	echo json_encode(array('message'=>'no record found','status'=>false));
+}
+//echo $data;
+?>
